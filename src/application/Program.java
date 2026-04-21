60a6e1a02b733acdebd080ef190edabb74cecb65
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-21 12:59:15 -0300

    Modifiquei o SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,9 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[]args) {
@@ -19,6 +21,13 @@ public class Program {
        Seller seller = sellerDao.findById(3);
        System.out.println(seller);
 
+       System.out.println("\n === TEST 2: seller findByDepartment ===");
+       Department department = new Department(2, null);
+       List<Seller> list = sellerDao.findByDepartment(department);
+
+        for( Seller obj: list) {
+            System.out.println(obj);
+        }
 
     }
 }
